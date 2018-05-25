e720cac19ee9743a7ecbb1d1aad5399c9191d3c6
Arlandis Word <arlandislawrence@gmail.com>
2018-05-25 08:14:57 -0400

    Hmm, still segfaulting

--- main.c
+++ main.c
@@ -12,15 +12,16 @@ float strikeout_percentage(struct player p) {
 
 int main(int argc, const char * argv[]) {
   printf("Player Name\tEPA\tK%%\n");
-  FILE * file = fopen(argv[1], "r");
-  char * player_string;
+  FILE *file = fopen(argv[1], "r");
+  char player_string[1000];
   while (fgets(player_string, 1000, file) != NULL) {
-    char * stat;
+    char *stat;
     struct player p;
+    strsep(&player_string, ",");
     while ((stat = strsep(&player_string, ",") != NULL)) {
       printf("Stat is %s", stat);
     }
-    //printf("%s\t%d\t%f\n", p.name, effective_plate_appearances(p), strikeout_percentage(p));
+    printf("%s\t%d\t%f\n", p.name, effective_plate_appearances(p), strikeout_percentage(p));
   }
   return 0;
 }
