9964d1311c1d9e6a5c8e8c1c873bf31d646f77c4
Arlandis Word <arlandislawrence@gmail.com>
2018-05-25 16:10:47 -0400

    Add a bunch of stats

--- player.h
+++ player.h
@@ -1,8 +1,33 @@
 struct player {
   char name[100];
-  int base_on_balls;
+  int age;
+  char team[4];
+  char league[2];
+  int g;
+  int plate_appearances;
   int at_bat;
+  int runs;
+  int hits;
+  int doubles;
+  int triples;
+  int home_runs;
+  int runs_batted_in;
+  int stolen_bases;
+  int caught_stealing;
+  int walks;
   int strikeouts;
+  int base_on_balls;
+  float ba;
+  float onbase_percentage;
+  float slugging_percentage;
+  float onbase_plus_slugging;
+  int ops_plus;
+  int total_bases;
+  int gdp; // gross domestic baseball product
+  int hit_by_pinch;
+  int sh;
+  int sacrifice_fly;
+  int intentional_walk;
 };
 
 int effective_plate_appearances(struct player p);
