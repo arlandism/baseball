49a04dce77f3890fc5dca667df1c2545a5d065ce
Arlandis Word <arlandislawrence@gmail.com>
2018-05-25 15:54:01 -0400

    Fix segfault and decimal formatting

--- main.c
+++ main.c
@@ -14,13 +14,14 @@ int main(int argc, const char * argv[]) {
   printf("Player Name\tEPA\tK%%\n");
   FILE *file = fopen(argv[1], "r");
   char player_string[1000];
+  char *ps_ptr = player_string;
   while (fgets(player_string, 1000, file) != NULL) {
     char *stat;
     struct player p;
-    while ((stat = strsep(&player_string, ",") != NULL)) {
-      printf("Stat is %s", stat);
+    while ((stat = strsep(&ps_ptr, ",")) != NULL) {
+      printf("Stat is %s\n", stat);
     }
-    printf("%s\t%d\t%f\n", p.name, effective_plate_appearances(p), strikeout_percentage(p));
+    printf("%s\t%d\t%.1f\n", p.name, effective_plate_appearances(p), strikeout_percentage(p));
   }
   return 0;
 }
