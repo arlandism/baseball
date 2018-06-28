e1d168c8fb46fc0cec186d5e2ac60bce7dd1e4a1
Arlandis Word <arlandislawrence@gmail.com>
2018-06-28 12:11:41 -0400

    Flesh out the queue implementation

--- queue.c
+++ queue.c
@@ -3,14 +3,24 @@
 #include "list.h"
 #include <stdlib.h>
 
+queue* initialize_queue() {
+  queue* q = malloc(sizeof(queue));
+  q->list = NULL;
+  return q;
+}
+
 void enqueue(queue *q, void *item) {
   insert(&q->list, item);
 }
 
 void * dequeue(queue *q) {
   list *tail = q->list;
+  // empty case
+  if (tail == NULL) return NULL;
+
+  // advance to the end of the list
   while (tail->next != NULL)
     tail = tail->next;
-  if (tail) return tail->item;
-  return NULL;
+
+  return tail->item;
 }
